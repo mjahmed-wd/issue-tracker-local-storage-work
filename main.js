ecad06d969f4e353343907fccd8e47522d613895
Jubair Ahmed <mjahmed.wd@gmail.com>
2021-02-13 14:55:03 +0600

    null fixed

--- main.js
+++ main.js
@@ -42,19 +42,22 @@ const fetchIssues = () => {
     const issuesList = document.getElementById('issuesList');
     issuesList.innerHTML = '';
 
-    for (var i = 0; i < issues.length; i++) {
-        const { id, description, severity, assignedTo, status } = issues[i];
+    if (issues !== null) {
+        for (var i = 0; i < issues.length; i++) {
+            const { id, description, severity, assignedTo, status } = issues[i];
 
-        issuesList.innerHTML += `<div class="well">
-                              <h6>Issue ID: ${id} </h6>
-                              <p><span class="label label-info"> ${status} </span></p>
-                              <h3 class="${status}"> ${description} </h3>
-                              <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
-                              <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="closeIssue('${id}')" class="btn btn-warning">Close</a>
-                              <a href="#" onclick="deleteIssue('${id}')" class="btn btn-danger">Delete</a>
-                              </div>`;
+            issuesList.innerHTML += `<div class="well">
+                                  <h6>Issue ID: ${id} </h6>
+                                  <p><span class="label label-info"> ${status} </span></p>
+                                  <h3 class="${status}"> ${description} </h3>
+                                  <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
+                                  <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
+                                  <a href="#" onclick="closeIssue('${id}')" class="btn btn-warning">Close</a>
+                                  <a href="#" onclick="deleteIssue('${id}')" class="btn btn-danger">Delete</a>
+                                  </div>`;
+        }
+        const openedIssues = issues.filter(issue => issue.status == 'Open');
+        document.getElementById('issueCount').innerText = `Issue Tracker: ${openedIssues.length}/${issues.length}`
     }
-    const openedIssues = issues.filter(issue => issue.status == 'Open');
-    document.getElementById('issueCount').innerText = `Issue Tracker: ${openedIssues.length}/${issues.length}`
+
 }
